89ca10c8967ad0e478e3023a8344b97cb658aab6
vinayakAtGiftbit <vinayak@giftbit.com>
2025-05-11 14:56:40 -0700

    feat: add-SendMagicLinkCall

--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.24.3
 
 require (
 	github.com/joho/godotenv v1.5.1
-	github.com/stytchauth/stytch-go/v15 v15.11.0
+	github.com/stytchauth/stytch-go/v16 v16.20.0
 )
 
 require (
 	github.com/MicahParks/keyfunc/v2 v2.0.1 // indirect
-	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
+	"os"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
-	"driftGo/domains/auth"
+	"driftGo/api/auth"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	http.HandleFunc("/auth/create", auth.CreateUserHandler)
-
+func main() {	
+	http.HandleFunc("/auth/create", auth.SendMagicLinkCall)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
 	log.Println("Server running on port", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
