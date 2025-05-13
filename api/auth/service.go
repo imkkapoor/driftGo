f546091a2d35b36d478ed81aa39de1dcaa7457e1
vinayakAtGiftbit <vinayak@giftbit.com>
2025-05-13 16:54:47 -0700

    feat: add authentication for magic links and refactor the code a bit with better type support

--- api/auth/service.go
+++ api/auth/service.go
@@ -1,29 +1,39 @@
 package auth
 
 import (
-    "context"
-    "log"
+	"context"
+	"log"
+	"os"
 
-    "driftGo/config"
+	"driftGo/config"
 
-    "github.com/stytchauth/stytch-go/v16/stytch/consumer/magiclinks/email"
-    "github.com/stytchauth/stytch-go/v16/stytch/consumer/stytchapi"
+	"github.com/stytchauth/stytch-go/v16/stytch/consumer/magiclinks"
+	"github.com/stytchauth/stytch-go/v16/stytch/consumer/magiclinks/email"
+	"github.com/stytchauth/stytch-go/v16/stytch/consumer/stytchapi"
 )
 
 var serviceClient *stytchapi.API
 
 func init() {
-    var err error
-    serviceClient, err = stytchapi.NewClient(config.ProjectID, config.Secret)
-    if err != nil {
-        log.Fatalf("failed to initialize Stytch client in service: %v", err)
-    }
+	var err error
+	serviceClient, err = stytchapi.NewClient(config.ProjectID, config.Secret)
+	if err != nil {
+		log.Fatalf("failed to initialize Stytch client in service: %v", err)
+	}
 }
 
 // SendMagicLink sends a magic link to the provided email address.
 func SendMagicLink(ctx context.Context, emailAddr string) (*email.SendResponse, error) {
-    params := &email.SendParams{
-        Email: emailAddr,
-    }
-    return serviceClient.MagicLinks.Email.Send(ctx, params)
+	params := &email.SendParams{
+		Email:             emailAddr,
+		LoginMagicLinkURL: os.Getenv("STYTCH_LOGIN_REDIRECT_URL"),
+	}
+	return serviceClient.MagicLinks.Email.Send(ctx, params)
+}
+
+func AuthenticateMagicLink(ctx context.Context, token string) (*magiclinks.AuthenticateResponse, error) {
+	params := &magiclinks.AuthenticateParams{
+		Token: token,
+	}
+	return serviceClient.MagicLinks.Authenticate(ctx, params)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module driftGo
 go 1.24.3
 
 require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/gorilla/schema v1.4.1
 	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
 	github.com/stytchauth/stytch-go/v16 v16.20.0
 )
 
@@ -11,4 +14,5 @@ require (
 	github.com/MicahParks/keyfunc/v2 v2.0.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
