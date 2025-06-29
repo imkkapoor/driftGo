54986a1a2af29ed70256967c92b6f27dc4ad9663
vinayakAtGiftbit <vinayak@giftbit.com>
2025-06-29 10:48:26 -0700

    feat: Add goose, sqlc and make

--- domain/link/service.go
+++ domain/link/service.go
@@ -3,6 +3,8 @@ package link
 import (
 	"context"
 	"driftGo/api/common/utils"
+	"driftGo/domain/user"
+	"errors"
 
 	"github.com/plaid/plaid-go/v35/plaid"
 )
@@ -11,20 +13,31 @@ import (
 Service handles all Plaid link-related operations
 */
 type Service struct {
-	client *plaid.APIClient
+	client      *plaid.APIClient
+	userService user.UserInterface
 }
 
 /*
 NewService creates a new link service with the provided Plaid credentials
 */
-func NewService(clientID, secret, env string) (*Service, error) {
+func NewService(clientID, secret, env string, userService user.UserInterface) (*Service, error) {
+	var plaidEnv plaid.Environment
+	switch env {
+	case "sandbox":
+		plaidEnv = plaid.Sandbox
+	case "production":
+		plaidEnv = plaid.Production
+	default:
+		return nil, errors.New(" Invalid Plaid environment: " + env)
+	}
+
 	configuration := plaid.NewConfiguration()
 	configuration.AddDefaultHeader("PLAID-CLIENT-ID", clientID)
 	configuration.AddDefaultHeader("PLAID-SECRET", secret)
-	configuration.UseEnvironment(plaid.Environment(env))
+	configuration.UseEnvironment(plaidEnv)
 
 	client := plaid.NewAPIClient(configuration)
-	return &Service{client: client}, nil
+	return &Service{client: client, userService: userService}, nil
 }
 
 /*
@@ -59,6 +72,43 @@ func (s *Service) CreateLinkToken(ctx context.Context) (*LinkTokenCallResponse, 
 }
 
 /*
+CreateLinkTokenForUser creates a link token for a specific user by their Stytch ID.
+This demonstrates how to use the user service within the link service.
+*/
+func (s *Service) CreateLinkTokenForUser(ctx context.Context, stytchUserID string) (*LinkTokenCallResponse, error) {
+	// Get user from user service
+	user, err := s.userService.GetUserByStytchID(ctx, stytchUserID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Use the user's UUID for Plaid
+	plaidUser := plaid.LinkTokenCreateRequestUser{
+		ClientUserId: user.Uuid.String(),
+	}
+
+	request := plaid.NewLinkTokenCreateRequest(
+		"drift",
+		"en",
+		[]plaid.CountryCode{plaid.COUNTRYCODE_CA},
+		plaidUser,
+	)
+
+	request.SetProducts([]plaid.Products{plaid.PRODUCTS_AUTH, plaid.PRODUCTS_IDENTITY})
+
+	linkToken, _, err := s.client.PlaidApi.LinkTokenCreate(ctx).LinkTokenCreateRequest(*request).Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	return &LinkTokenCallResponse{
+		LinkToken:  linkToken.GetLinkToken(),
+		Expiration: linkToken.GetExpiration(),
+		RequestID:  linkToken.GetRequestId(),
+	}, nil
+}
+
+/*
 ExchangePublicToken exchanges a public token for an access token.
 This is used after a user successfully links their bank account through Plaid Link.
 The public token is exchanged for an access token that can be used to access the user's bank account data.
@@ -76,4 +126,63 @@ func (s *Service) ExchangePublicToken(ctx context.Context, publicToken string) (
 		ItemID:      response.GetItemId(),
 		RequestID:   response.GetRequestId(),
 	}, nil
+}
+
+/*
+GetInstitutionMetadata gets the institution ID and name for a given access token.
+*/
+func (s *Service) GetInstitutionMetadata(ctx context.Context, accessToken string) (institutionId string, institutionName string, err error) {
+	institutionId, err = s.GetInstitutionId(ctx, accessToken)
+	if err != nil {
+		return "", "", err
+	}
+
+	institutionName, err = s.GetInstitutionName(ctx, accessToken)
+	if err != nil {
+		return "", "", err
+	}
+
+	return institutionId, institutionName, nil
+}
+
+/*
+GetInstitutionId gets the institution ID for a given access token.
+*/
+func (s *Service) GetInstitutionId(ctx context.Context, accessToken string) (institutionId string, err error) {
+	request := plaid.NewItemGetRequest(accessToken)
+
+	response, _, err := s.client.PlaidApi.ItemGet(ctx).ItemGetRequest(*request).Execute()
+	if err != nil {
+		return "", err
+	}
+
+	return *response.GetItem().InstitutionId.Get(), nil
+}
+
+/*
+GetInstitutionName gets the institution name for a given access token.
+*/
+func (s *Service) GetInstitutionName(ctx context.Context, accessToken string) (institutionName string, err error) {
+	request := plaid.NewItemGetRequest(accessToken)
+
+	response, _, err := s.client.PlaidApi.ItemGet(ctx).ItemGetRequest(*request).Execute()
+	if err != nil {
+		return "", err
+	}
+
+	return *response.GetItem().InstitutionName.Get(), nil
+}
+
+/*
+GetAccounts gets the accounts for a given access token.
+*/
+func (s *Service) GetAccounts(ctx context.Context, accessToken string) ([]plaid.AccountBase, error) {
+	request := plaid.NewAccountsGetRequest(accessToken)
+
+	response, _, err := s.client.PlaidApi.AccountsGet(ctx).AccountsGetRequest(*request).Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	return response.GetAccounts(), nil
 }
--- go.mod
+++ go.mod
@@ -7,14 +7,16 @@ toolchain go1.24.3
 require (
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/go-playground/validator/v10 v10.19.0
-	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/schema v1.4.1
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 	github.com/plaid/plaid-go/v35 v35.0.0
+	github.com/pressly/goose/v3 v3.24.3
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stytchauth/stytch-go/v16 v16.21.0
+	github.com/svix/svix-webhooks v1.67.0
 )
 
 require (
@@ -24,22 +26,20 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/svix/svix-webhooks v1.67.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/net v0.38.0 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/oauth2 v0.18.0 // indirect
-	golang.org/x/sync v0.13.0 // indirect
-	golang.org/x/sys v0.32.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
